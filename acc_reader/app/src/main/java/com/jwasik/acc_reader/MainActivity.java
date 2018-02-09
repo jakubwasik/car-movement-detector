861a7e12def107289ed2c55158bce4b9a5ac6ead
Kuba Wasik <jakubwasik01@gmail.com>
2018-02-09 11:38:02 +0100

    version before another approach

--- acc_reader/app/src/main/java/com/jwasik/acc_reader/MainActivity.java
+++ acc_reader/app/src/main/java/com/jwasik/acc_reader/MainActivity.java
@@ -27,41 +27,47 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Handler mainHandler = new Handler(){
+    private Handler mainHandler = new Handler() {
         @Override
-        public void handleMessage(Message message){
-            bufferMainThread = (ArrayList<ArrayList<SensorData>>)message.obj;
+        public void handleMessage(Message message) {
+            bufferMainThread = (ArrayList<ArrayList<SensorData>>) message.obj;
             new DatabaseUploader(bufferMainThread.get(0), uploader).execute();
         }
     };
     private DataBaseManager uploader;
     public TCPClient client;
 
-    private class DatabaseUploader extends AsyncTask<Void,Void,Void>{
+    private class DatabaseUploader extends AsyncTask<Void, Void, Void> {
         private ArrayList<SensorData> data;
         private DataBaseManager uploader;
-        DatabaseUploader(ArrayList<SensorData> data, DataBaseManager uploader){
+
+        DatabaseUploader(ArrayList<SensorData> data, DataBaseManager uploader) {
             this.data = data;
             this.uploader = uploader;
         }
-        protected void onPreExecute(){
+
+        protected void onPreExecute() {
             pb.setVisibility(ProgressBar.VISIBLE);
         }
-        protected Void doInBackground(Void ... params){
+
+        protected Void doInBackground(Void... params) {
             int tableNr = uploader.getTableNumber();
             uploader.insertIntoResultsTable("SUCCESS");
-            uploader.fillDataTable(tableNr+1, this.data);
+            uploader.fillDataTable(tableNr + 1, this.data);
             return null;
         }
-        protected void onPostExecute(Void result){
+
+        protected void onPostExecute(Void result) {
             pb.setVisibility(ProgressBar.INVISIBLE);
         }
     }
+
     SensorManager manager;
-    TextView ax1,ax2,ax3;
+    TextView ax1, ax2, ax3;
     ProgressBar pb;
     ArrayList<ArrayList<SensorData>> bufferMainThread = new ArrayList<ArrayList<SensorData>>(50);
     DataAcquisition configurator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,41 +76,40 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         uploader = new DataBaseManager(this);
         pb = findViewById(R.id.progressBar);
-        HandlerThread connectionHandlerThread = new HandlerThread("TCP connection");
-        connectionHandlerThread.start();
-        final Handler connectionHandler = new Handler(connectionHandlerThread.getLooper());
-        HandlerThread test = new HandlerThread("TCP receive");
-        test.start();
-        final Handler testhandler = new Handler(test.getLooper());
         client = new TCPClient("192.168.1.5", 8888);
-        connectionHandler.post(client);
         Button sendToServer = findViewById(R.id.sendToServer);
-
+        Log.e("aaaaaaaaaaaaaaaaaaa", String.valueOf(Thread.activeCount()));
         sendToServer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                testhandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        client.sendMessage("message from handler");
-                    }
-                });
+                client.connect(TCPClient.TcpClientMode.FILE_MODE);
+                client.sendMessage("Halo tu klient!");
+                client.sendMessage("Halo tu klient po raz drugi!");
+
+                client.disconnect();
+                Log.e("aaaaaaaaaaaaaaaaaaa", client.connectionHT.getState().toString());
+                Log.e("aaaaaaaaaaaaaaaaaaa", String.valueOf(Thread.activeCount()));
             }
         });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 configurator.getHandler().sendEmptyMessage(0);
-               // for(String elem: runnable.buffer)
-                 //   Log.e("data", elem);
+                Log.e("aaaaaaaaaaaaaaaaaaa", client.connectionHT.getState().toString());
+                Log.e("aaaaaaaaaaaaaaaaaaa", client.disconnectionHT.getState().toString());
+                Log.e("aaaaaaaaaaaaaaaaaaa", client.sendHT.getState().toString());
+                // for(String elem: runnable.buffer)
+                //   Log.e("data", elem);
             }
         });
         ax2 = findViewById(R.id.ax2);
         ax3 = findViewById(R.id.ax3);
         configurator = new DataAcquisition(this, mainHandler);
         configurator.configure();
+
         //uploader.readTable();
     }
+
     protected void onResume() {
         super.onResume();
         configurator.onResume();
@@ -113,21 +118,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         configurator.onPause();
-    }
-    private class TCPConnection extends AsyncTask<Void,Void,Void>{
-
-        protected void onPreExecute(){
-            ;
-        }
-        protected Void doInBackground(Void ... params){
-            client = new TCPClient("192.168.1.5", 8888);
-            Log.e("doInBackground", Thread.currentThread().getName());
-            client.connect();
-            client.run();
-            return null;
-        }
-        protected void onPostExecute(Void result){
-            pb.setVisibility(ProgressBar.INVISIBLE);
-        }
     }
 }
--- acc_reader/app/src/main/java/com/jwasik/acc_reader/TCPClient.java
+++ acc_reader/app/src/main/java/com/jwasik/acc_reader/TCPClient.java
@@ -1,5 +1,7 @@
 package com.jwasik.acc_reader;
 
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -13,64 +15,165 @@ import java.io.*;
  * Created by kuba on 2018-02-01.
  */
 
-public class TCPClient implements Runnable{
+public class TCPClient{
 
     private String serverIP;
-    public int serverPort;
-    BufferedReader in;
-    BufferedWriter out;
+    private int serverPort;
+    private BufferedReader in;
+    private BufferedWriter out;
     private Socket socket;
     public boolean mRun;
+    private TcpClientState state = TcpClientState.DISCONNECTED;
+    public HandlerThread connectionHT;
+    public HandlerThread disconnectionHT;
+    public HandlerThread sendHT;
+    public HandlerThread receiveHT;
+    private Handler connectionHandler;
+    private Handler disconnectionHandler;
+    private Handler sendHandler;
+    private Handler receiveHandler;
+    private boolean readyToDisconnect = true;
 
+    public enum TcpClientState {
+        DISCONNECTED,
+        CONNECTING,
+        CONNECTED,
+        CONNECTION_STARTED,
+        FAILED
+    }
+    public enum TcpClientMode {
+        FILE_MODE,
+        DATA_MODE
+    }
     public TCPClient(String serverIP, int serverPort) {
         this.serverIP = serverIP;
         this.serverPort = serverPort;
     }
-    public void stopClient(){
-        mRun = false;
+
+    public void connect(TcpClientMode mode)
+    {
+        readyToDisconnect = false;
+        connectionHT = new HandlerThread("Connection with: " + serverIP);
+        connectionHT.start();
+        connectionHandler = new Handler(connectionHT.getLooper());
+        state = TcpClientState.CONNECTING;
+        connectionHandler.post(new Connect(mode));
+        connectionHT.quitSafely();
     }
-    public boolean connect(){
-        try{
-            InetAddress serverAddr = InetAddress.getByName(serverIP);
-            this.socket = new Socket(serverIP, serverPort);
-            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            return true;
-        } catch (Exception ex){
-            ex.printStackTrace();
-            return false;
+    public void sendMessage(String msg)
+    {
+        readyToDisconnect = false;
+        sendHT = new HandlerThread("Send msg to: " + serverIP);
+        sendHT.start();
+        sendHandler = new Handler(sendHT.getLooper());
+        sendHandler.post(new sendMessage(msg));
+        sendHT.quitSafely();
+    }
+    public void disconnect()
+    {
+        disconnectionHT = new HandlerThread("Disconnection thread");
+        disconnectionHT.start();
+        disconnectionHandler = new Handler(disconnectionHT.getLooper());
+        disconnectionHandler.post(new Disconnect());
+        disconnectionHT.quitSafely();
+    }
+    private class Disconnect implements Runnable{
+        @Override
+        public void run(){
+            while(!readyToDisconnect);
+            state = TcpClientState.DISCONNECTED;
         }
     }
-    public void sendMessage(String message){
-        if (out != null) {
+    private class Connect implements Runnable{
+        private TcpClientMode mode;
+        Connect(TcpClientMode mode){
+            this.mode = mode;
+        }
+        @Override
+        public void run(){
             try{
-                out.write(message);
-                out.flush();
-                Log.e("from server", Thread.currentThread().getName());
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
-        }
-    }
-    public void run(){
-        this.connect();
-        mRun = true;
-        sendMessage("tutaj klient");
-        try{
-            while (mRun) {
-                String serverMessage = in.readLine();
-
-                if (serverMessage != null) {
-                    //call the method messageReceived from MyActivity class
-                    Log.e("from server - thread:", Thread.currentThread().getName());
-                    Log.e("from server", serverMessage);
+                if(state != TcpClientState.CONNECTED) {
+                    Log.e("Connected runnable", Thread.currentThread().getName());
+                    InetAddress serverAddr = InetAddress.getByName(serverIP);
+                    socket = new Socket(serverIP, serverPort);
+                    out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    Log.e("pierwsze connected", Thread.currentThread().getName());
+                    state = TcpClientState.CONNECTED;
+                    String modeToSend;
+                    if(this.mode == TcpClientMode.FILE_MODE)
+                        modeToSend = "Send File";
+                    else
+                        modeToSend = "Send Continuous Data";
+                    out.write(modeToSend);
+                    out.flush();
                 }
-                serverMessage = null;
+                else{
+                    Log.e("Connection Exists!", Thread.currentThread().getName());
+                }
+                while (state == TcpClientState.CONNECTED) {
+                    //String serverMessage = in.readLine();
+                    //if (serverMessage != null) {
+                        //call the method messageReceived from MyActivity class
+                     //   Log.e("DEBUGGING THREAD", Thread.currentThread().getName());
+                     //   Log.e("Received from server", serverMessage);
+                    //}
+                    //readyToDisconnect = true;
+                  //  serverMessage = null;
+                }
+                Log.e("DISCONNECTING", Thread.currentThread().getName());
+                in.close();
+                out.flush();
+                out.close();
+                socket.close();
+            }catch (Exception ex){
+                ex.printStackTrace();
+                state = TcpClientState.FAILED;
             }
-            socket.close();
-        }catch (Exception ex){
-            ex.printStackTrace();
         }
     }
+    private class sendMessage implements Runnable{
+        private String message;
+        sendMessage(String msg){
+            this.message = msg;
+        }
+        @Override
+        public void run(){
+            while(state != TcpClientState.CONNECTED);
+            if (true) {
+                try{
+                    int msgSize = message.length();
+                    Log.e("przed wyslaniem", message);
+                    out.write("Wysylam CSV: " + String.valueOf(msgSize));
+                    out.flush();
+                    char[] buffer = new char[1024];
+                    int charsReaded = in.read(buffer, 0,  1024);
+                    String bufferStr = new String(buffer, 0, charsReaded);
 
+                    Log.e("Ile odczytalo znakow", String.valueOf(bufferStr));
+                    if(Integer.parseInt(bufferStr) == msgSize){
+                        out.write(message, 0 ,msgSize);
+                        out.flush();
+                    }
+                    Log.e("from server", Thread.currentThread().getName());
+
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                    state = TcpClientState.FAILED;
+                }
+            }
+            readyToDisconnect = true;
+        }
+    }
+   /* private class ReceiveFromServer implements Runnable{
+        @Override
+        public void run(){
+            try{
+
+            }catch (Exception ex){
+                ex.printStackTrace();
+                state = TcpClientState.FAILED;
+            }
+        }
+    }*/
 }
